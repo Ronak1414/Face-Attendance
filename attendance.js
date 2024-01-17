5ad421fcfd3eb29fe6a9223fb3eaaee61aaf7044
Ronak <ronakofficial1414@gmail.com>
2024-01-17 22:39:18 +0530

    new file:   attendance.css 	new file:   attendance.html 	new file:   attendance.js

---
+++ attendance.js
@@ -0,0 +1,12 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycby4JfRRwdj0EKFJmIsrH02KVbtfaCk1OKCH5nv4KLCSynRsXRAGLCTxnQF_HJCfkqE/exec'
+
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
